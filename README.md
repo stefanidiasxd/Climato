7dccc1c36385345cf6ef81a943263771863883e2
stefanixd <stefanidsdias@gmail.com>
2025-11-05 19:53:00 -0300

    test

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# Projeto Clima-App
+
+Este é  o projeto Clima-App, uma página de status do tempo desenvolvida utilizando a API OpenWeatherMap. Neste projeto, criei uma página simples e responsiva que permite aos usuários buscar a previsão do tempo de uma cidade e exibe informações como temperatura, velocidade do vento e condições do céu.
+
+
+
+
+https://github.com/aureliodeboa/Clima-app/assets/53971991/572e87e8-138b-4464-84d8-5457db401789
+
+
+## Descrição do Projeto
+
+O Projeto Clima-App é uma aplicação web que utiliza a API OpenWeatherMap para fornecer informações status do tempo em tempo real para uma cidade específica. Os usuários podem inserir o nome da cidade na caixa de pesquisa e receberão informações atualizadas sobre o clima naquela localidade.
+
+## Funcionalidades Principais
+
+- Busca de previsão do tempo com base no nome da cidade.
+- Exibição de informações como temperatura, velocidade do vento e condições do céu.
+- Design responsivo que se adapta a diferentes tamanhos de tela.
+
+## Tecnologias Utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
+- API OpenWeatherMap
+
+## Instalação
+
+Não é necessário instalação para este projeto. Basta abrir o arquivo `index.html` em um navegador da web para acessar a página de previsão do tempo.
+
+```bash
+# Clone o repositório (caso esteja utilizando um sistema de controle de versão)
+git clone https://github.com/aureliodeboa/Clima-app.git
+
+# Navegue até o diretório do projeto
+cd nome-do-repo
+```
+
+## Como Usar
+
+1. Abra o arquivo `index.html` em seu navegador da web.
+2. Na caixa de pesquisa, digite o nome da cidade para a qual deseja obter a previsão do tempo.
+3. Pressione o botão de pesquisa ou pressione "Enter" no teclado.
+4. As informações de previsão do tempo para a cidade especificada serão exibidas na tela, incluindo temperatura, velocidade do vento e condições do céu.
+
+## Contribuição
+
+Este é um projeto pessoal. Portantom fico feliz em ouvir seus comentários e sugestões!
+
---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+// Cria uma tela inicial dinamicamente
+
+
+
+
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+    if(input !=='')
+    {
+        clearInfo();    
+        showWarning('Carregando...')
+        
+
+        let url= `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`
+       
+        let results = await fetch(url);
+        let json = await results.json();
+      
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+        
+                descri : json.weather[0].description,
+
+            } 
+            );
+        }
+        else{
+            clearInfo();
+            showWarning('Não encontramos essa localização');
+        }
+    }else{
+        clearInfo();
+    }
+})
+
+
+function showInfo(json){
+    showWarning('');
+    document.querySelector('.resultado').style.display= 'block';
+    document.querySelector('.titulo').innerHTML= `${json.name}, ${json.country}`
+    document.querySelector('.temperatura').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+    document.querySelector('.tempInfo').innerHTML= `${json.descri} `;
+    document.querySelector('.informacoes img').setAttribute('src',`assets/images/${json.tempIcon}.gif`);
+
+
+}
+
+function showWarning(msg)
+{
+    document.querySelector('.aviso').innerHTML = msg
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
+
+ async function ijuí (){
+    let input = 'Ijuí'
+    if(input !=='')
+    {
+        clearInfo();    
+        showWarning('Carregando...')
+        
+
+        let url= `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ef60a79c9c3ca99f2edfad01fd9badb3&units=metric&lang=pt_br`
+       
+        let results = await fetch(url);
+        let json = await results.json();
+      
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+        
+                descri : json.weather[0].description,
+
+            } 
+            );
+        }
+        else{
+            clearInfo();
+            showWarning('Não encontramos essa localização');
+        }
+    }else{
+        clearInfo();
+    }
+}
+
+ijuí();
